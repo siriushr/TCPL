dbb0c7bb777940f35582eaddb8e1e829a9841f09
siriushr <siriuslemon@gmail.com>
2015-10-13 16:02:26 +0800

    modified itoa.cpp

--- itoa.cpp
+++ itoa.cpp
@@ -1,14 +1,14 @@
+#define abs(x) ((x) < 0? -(x) : (x))
 /* itoa函数：将数字n转换为字符串并保存到s中 */
 
 void itoa(int n, char s[]) {
 	int i, sign;
 
-	if ((sign = n) < 0) //记录符号
-		n = -n;  //使n成为正数
+	sign = n;
 	i = 0;
-	do {   //以反序生成数字
-		s[i++] = n % 10 + '0'; //取下一个数字
-	} while ((n /= 10) > 0);   //删除该数字
+	do {
+		s[i++] = abs(n % 10) + '0';
+	} while ((n /= 10) != 0) 
 
 	if (sign < 0)
 		s[i++] = '-';
